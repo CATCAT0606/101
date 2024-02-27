801ccc88e5a7b4548f9342cc3c2b58f8ca904210
CATCAT0606 <127452504+CATCAT0606@users.noreply.github.com>
2024-02-27 17:56:48 -0500

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDqAs53yqrUCqvlUlOnVy35b5ZsLdwoOP8",
+    authDomain: "c100-f6328.firebaseapp.com",
+    databaseURL: "https://c100-f6328-default-rtdb.firebaseio.com",
+    projectId: "c100-f6328",
+    storageBucket: "c100-f6328.appspot.com",
+    messagingSenderId: "1090635728030",
+    appId: "1:1090635728030:web:cf95338c26eaafb338678f"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
